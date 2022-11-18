2f931eee7be9eeb4e9366fe4c30d7aba92ff3e7f
kumar.vishal <kumar.vishal@CPU-0104>
2022-11-18 11:18:50 +0530

    ahsh

---
+++ EmpMgmtUsingDB.Entity/DummyAPIUtilityModel/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpMgmtUsingDB.Model.DummyAPIUtilityModel
+{
+    public class Entity
+    {
+        //public readonly int Id { get;  }
+    }
+}
